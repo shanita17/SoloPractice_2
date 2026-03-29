a778fb865e68d762cf36a1f146b1cc1f8af2267a
lucythefuckingweirdo <46873278+lucythefuckingweirdo@users.noreply.github.com>
2026-03-29 03:33:31 +0200

    Added HTML intergration, CSS design & Javascript functionality. This code is meant for 1 die.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Random number generator
-
+/* NOTES
 let randomNum = Math.floor(Math.random() * 6) + 1; //This function utilises the Math function "Random", 
                                 // to generate random numbers. (Between 0 - 1)
                                 //For the example here, we used a die (1 - 6)
@@ -9,3 +9,16 @@ let randomNum = Math.floor(Math.random() * 6) + 1; //This function utilises the 
                                 // we add the 1 to move the starting post from 0 - 5 to 1 - 6
 
 console.log(randomNum);
+*/
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+
+const min = 1;
+const max = 6;
+
+let randomNum;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
+}
