bcbd4a55990f4092493b5124fcc8871574986d98
lucythefuckingweirdo <46873278+lucythefuckingweirdo@users.noreply.github.com>
2026-03-29 03:13:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Random number generator
+
+let randomNum = Math.floor(Math.random() * 6) + 1; //This function utilises the Math function "Random", 
+                                // to generate random numbers. (Between 0 - 1)
+                                //For the example here, we used a die (1 - 6)
+                                // We multiple play Math.Random by 6 in order to get numbers between
+                                // 1 - 6 exclusively. To remove the decimals, 
+                                // we just used a another Math function "Round" to round down the number
+                                // we add the 1 to move the starting post from 0 - 5 to 1 - 6
+
+console.log(randomNum);
